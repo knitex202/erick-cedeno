4712b8ff34d8928f53f57ad77b14218a454bcaef
knitex202 <99418580+knitex202@users.noreply.github.com>
2024-06-05 20:25:43 -0500

    fixed mobile issues

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,12 +5,12 @@ import { data } from "../data";
 
 function Landing() {
   return (
-    <div className="w-full h-full">
+    <div className="flex flex-col w-full h-full">
       <div className="flex flex-col lg:flex lg:flex-row lg:justify-center mt-8 ml-12 items-center">
         <div className="">
-        <h1 className=" inline-block text-6xl ">
-          Erick <span className="text-5xl font-thin">Cedeño</span>
-        </h1>
+          <h1 className=" inline-block text-6xl ">
+            Erick <span className="text-5xl font-thin">Cedeño</span>
+          </h1>
         </div>
         <div className="relative top-1 ml-6">
           <a href="https://www.instagram.com/mr.erickcedeno/" target="blank">
@@ -20,41 +20,45 @@ function Landing() {
       </div>
       <div className=" my-16 flex w-full">
         <div className="flex w-full relative overflow-x-scroll">
-        <Card info={data} />
+          <Card info={data} />
         </div>
       </div>
-      <div className="flex w-full justify-evenly text-xs lg:text-lg text-gray-600">
-        <div>
-          <p>
-            HEIGHT: <span className="font-bold text-black">5'10" / 178 cm</span>
-          </p>
-          <p>
-            COAT: <span className="font-bold text-black">40 Regular</span>
-          </p>
-        </div>
-        <div>
-          <p>
-            NECK: <span className="font-bold text-black">15½</span>{" "}
-          </p>
-          <p>
-            SHOE: <span className="font-bold text-black">10½</span>
-          </p>
-        </div>
-        <div>
-          <p>
-            SLEEVE: <span className="font-bold text-black">33</span>{" "}
-          </p>
-          <p>
-            HAIR: <span className="font-bold text-black">Grey</span>
-          </p>
-        </div>
-        <div>
-          <p>
-            WAIST/INSEAM: <span className="font-bold text-black">32 / 30</span>{" "}
-          </p>
-          <p>
-            EYES: <span className="font-bold text-black">Brown</span>{" "}
-          </p>
+      <div className="flex flex-row w-2/3 self-center">
+        <div className="grid grid-cols-2 lg:grid-cols-4 w-full text-xs lg:text-lg text-gray-600">
+          <div>
+            <p>
+              HEIGHT:{" "}
+              <span className="font-bold text-black">5'10" / 178 cm</span>
+            </p>
+            <p>
+              COAT: <span className="font-bold text-black">40 Regular</span>
+            </p>
+          </div>
+          <div>
+            <p>
+              NECK: <span className="font-bold text-black">15½</span>{" "}
+            </p>
+            <p>
+              SHOE: <span className="font-bold text-black">10½</span>
+            </p>
+          </div>
+          <div>
+            <p>
+              SLEEVE: <span className="font-bold text-black">33</span>{" "}
+            </p>
+            <p>
+              HAIR: <span className="font-bold text-black">Grey</span>
+            </p>
+          </div>
+          <div>
+            <p>
+              WAIST/INSEAM:{" "}
+              <span className="font-bold text-black">32 / 30</span>{" "}
+            </p>
+            <p>
+              EYES: <span className="font-bold text-black">Brown</span>{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
