e11909dd22f74eab23908ab57b5b729a5b0d977f
knitex202 <99418580+knitex202@users.noreply.github.com>
2024-06-23 13:17:20 -0700

    changed the design of the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from "./logo.svg";
-
-import Landing from "./components/Landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Landing from "./Pages/Landing";
+import Campaigns from "./Pages/Campaigns";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
-    <div className="w-screen h-screen md:h-full md:overflow-x-hidden h-695:overflow-y-scroll bg-gray-200 overflow-hidden">
-      <Landing/>
+    <div className="flex flex-col w-full h-full bg-stone-950 ">
+      <div className="w-full h-full object-cover">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/campaigns" element={<Campaigns />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,49 +1,106 @@
-import erickMug from "./assets/erickMug.png";
-import erickShots1 from "./assets/erickShots1.png";
-import erickSnow from "./assets/erickSnow.jpg";
-import erickSnow2 from "./assets/erickSnow2.jpg";
-import erickSnow3 from "./assets/erickSnow3.jpg";
-import erickShots2 from "./assets/erickShots2.png";
-import erickShots3 from "./assets/erickShots3.png";
-import erickShots4 from "./assets/erickShots4.png";
-import erickShots5 from "./assets/erickShots5.png";
-import erickShots6 from "./assets/erickShots6.png";
-import erickShots7 from "./assets/erickShots7.png";
+import erickOffice from "./Assets/galleryImages/erick-office.jpg";
+import erickPhoneAirport from "./Assets/galleryImages/erick-phone-airport.jpg";
+import kingWalk from './Assets/galleryImages/Walk-like-a-king.jpg';
+import bikeHead from './Assets/galleryImages/erick-suit-bicycle.jpg';
+import tableSitting from './Assets/galleryImages/sitting-on-table.jpg';
+import suitwalking from './Assets/galleryImages/erick-suit-walking.jpg';
+import snow1 from './Assets/galleryImages/erickSnow.jpg';
+import snow2 from './Assets/galleryImages/erickSnow2.jpg';
+import snow3 from './Assets/galleryImages/erickSnow3.jpg';
 
-export const data = [
+// Marley Campaign
+import marley1 from './Assets/galleryImages/marley1.jpg';
+import marley2 from './Assets/galleryImages/marley2.jpg';
+import marley3 from './Assets/galleryImages/marley3.jpg';
+import marley4 from './Assets/galleryImages/marley4.jpg';
+import marley5 from './Assets/galleryImages/marley5.jpg';
+import marley6 from './Assets/galleryImages/marley6.jpg';
+import marley7 from './Assets/galleryImages/marley7.jpg';
+import marley8 from './Assets/galleryImages/marley8.jpg';
+import marley9 from './Assets/galleryImages/marley9.jpg';
+
+// Nike Jordan Campaign
+import nike1 from './Assets/galleryImages/nike1.jpg';
+import nike2 from './Assets/galleryImages/nike2.jpg';
+import nike3 from './Assets/galleryImages/nike3.jpg';
+import nike4 from './Assets/galleryImages/nike4.jpg';
+import nike5 from './Assets/galleryImages/nike5.jpg';
+
+export const Images = [
     {
-        image: erickMug
+        picture: erickOffice, alt: "Office Modeling"
     },
     {
-        image: erickShots1
+        picture: erickPhoneAirport, alt: "Office in airport"
     },
     {
-        image: erickSnow
+        picture: kingWalk, alt: "Walking like a king"
     },
     {
-        image: erickSnow2
+        picture: suitwalking, alt: "Walking in a suit"
     },
     {
-        image: erickSnow3
+        picture: tableSitting, alt: "Sitting on a table"
     },
     {
-        image: erickShots2
+        picture: bikeHead, alt: "Explorers club"
     },
     {
-        image: erickShots3
+        picture: snow1, alt: "Snow day sitting and smiling"
     },
     {
-        image: erickShots4
+        picture: snow2, alt: "Snow day side view"
     },
     {
-        image: erickShots5
+        picture: snow3, alt: "Snow day standing and smiling"
+    },
+]
+
+
+export const marleyImages = [
+    {
+        picture: marley1, alt: "Marley Campaign"
     },
     {
-        image: erickShots6
+        picture: marley2, alt: "Marley Campaign"
     },
     {
-        image: erickShots7
+        picture: marley3, alt: "Marley Campaign"
     },
-    
-    
+    {
+        picture: marley4, alt: "Marley Campaign"
+    },
+    {
+        picture: marley5, alt: "Marley Campaign"
+    },
+    {
+        picture: marley6, alt: "Marley Campaign"
+    },
+    {
+        picture: marley7, alt: "Marley Campaign"
+    },
+    {
+        picture: marley8, alt: "Marley Campaign"
+    },
+    {
+        picture: marley9, alt: "Marley Campaign"
+    },
+]
+
+export const nikeJordanImages = [
+    {
+        picture: nike1, alt: "Nike Jordan Campaign"
+    },
+    {
+        picture: nike2, alt: "Nike Jordan Campaign"
+    },
+    {
+        picture: nike3, alt: "Nike Jordan Campaign"
+    },
+    {
+        picture: nike4, alt: "Nike Jordan Campaign"
+    },
+    {
+        picture: nike5, alt: "Nike Jordan Campaign"
+    },
 ]
